ba7eb1a8e351e98546d853b066a74421a52db207
aayambhatt <aayambhatt23@gmail.com>
2025-02-22 22:52:18 +0530

    buy sell stocks pt1

---
+++ 01-Array-String/07-Buy-Sell-Stocks-pt-1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+    int mini = prices[0];
+    int profit=0;
+    int n = prices.size();
+
+    for(int i =0; i<n ; i++){
+        int cost = prices[i]-mini;
+        profit = max(profit, cost);
+
+        mini = min(mini, prices[i]);
+    }
+
+    return profit;
+
+}
+
+int main()
+{
+ return 0;
+}
